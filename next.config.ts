a9e0ed6b0dc202eb4cda5fe0f5643b74606dbdd7
fujiwarajukito <fujiwarajukito@example.com>
2026-01-05 21:39:11 +0900

    111

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.dicebear.com',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
